0443256cab20018c27ad6a34ad6377ef63fc3674
seregamorph <serega.morph@gmail.com>
2024-01-24 21:48:41 +0100

    junit5 extract className

--- spring-test-smart-context/src/main/java/com/github/seregamorph/testsmartcontext/CurrentTestContextExecutionListener.java
+++ spring-test-smart-context/src/main/java/com/github/seregamorph/testsmartcontext/CurrentTestContextExecutionListener.java
@@ -1,6 +1,8 @@
 package com.github.seregamorph.testsmartcontext;
 
 import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.engine.TestSource;
+import org.junit.platform.engine.support.descriptor.ClassSource;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 
@@ -8,12 +10,26 @@ public class CurrentTestContextExecutionListener implements TestExecutionListene
 
     @Override
     public void executionStarted(TestIdentifier testIdentifier) {
-        CurrentTestContext.setCurrentTestClassIdentifier(testIdentifier.getSource()
-            .map(Object::toString).orElse("undefined"));
+        CurrentTestContext.setCurrentTestClassIdentifier(getTestClassName(testIdentifier));
     }
 
     @Override
     public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
         CurrentTestContext.resetCurrentTestClassIdentifier();
+    }
+
+    private static String getTestClassName(TestIdentifier testIdentifier) {
+        return testIdentifier.getSource()
+            .map(CurrentTestContextExecutionListener::getTestClassName)
+            .map(Object::toString).orElse("undefined");
+    }
+
+    private static String getTestClassName(TestSource source) {
+        // in the common case it can be not the class name
+        if (source instanceof ClassSource) {
+            ClassSource classSource = (ClassSource) source;
+            return classSource.getClassName();
+        }
+        return source.toString();
     }
 }
